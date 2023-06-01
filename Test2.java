8756905251966600b22104439fd0cc675c8446e7
MdAmjad123 <125453089+MdAmjad123@users.noreply.github.com>
2023-06-01 23:12:43 +0530

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,14 @@
+   package booleanliteral;
+
+    public class Test2 {
+
+	public static void main(String[] args) 
+	{
+		
+   // boolean a = 0;  // b/c integer value 
+   // boolean b= 1 ;	//b/c integer value 
+   //System.out.println(a);
+   //System.out.println(b);
+
+   }
+   }
